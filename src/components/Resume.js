647e7ccc4fa855df959e1297efd780f9c02040f7
Remy Guts <remyguts@gmail.com>
2020-04-20 13:10:28 -0700

    added styling directories

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -15,18 +15,18 @@ class Resume extends Component {
           </div>
         );
       });
-      var work = this.props.data.work.map(function (work) {
-        return (
-          <div key={work.company}>
-            <h3>{work.company}</h3>
-            <p className="info">
-              {work.title}
-              <span>&bull;</span> <em className="date">{work.years}</em>
-            </p>
-            <p>{work.description}</p>
-          </div>
-        );
-      });
+      //var work = this.props.data.work.map(function (work) {
+      //return (
+      //<div key={work.company}>
+      //<h3>{work.company}</h3>
+      //<p className="info">
+      // {work.title}
+      // <span>&bull;</span> <em className="date">{work.years}</em>
+      //</p>
+      //<p>{work.description}</p>
+      //</div>
+      //);
+      //});
       var skills = this.props.data.skills.map(function (skills) {
         var className = "bar-expand " + skills.name.toLowerCase();
         return (
@@ -53,15 +53,12 @@ class Resume extends Component {
             </div>
           </div>
         </div>
-
         <div className="row work">
           <div className="three columns header-col">
             <h1>
               <span>Professional</span>
             </h1>
           </div>
-
-          <div className="nine columns main-col">{work}</div>
         </div>
 
         <div className="row skill">

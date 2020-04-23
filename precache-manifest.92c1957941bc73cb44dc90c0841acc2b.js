36d16d462a8488ddd7a07a7324e2c1358814c5d8
Remy Guts <remyguts@gmail.com>
2020-04-22 17:39:57 -0700

    Updates

---
+++ precache-manifest.92c1957941bc73cb44dc90c0841acc2b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bd0b53484b8d24ee80207917dd4cf43",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "3f56d3b9606ef6309dd4",
+    "url": "/portfolio-react/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "ad35e04abed861196355",
+    "url": "/portfolio-react/static/css/main.951f35fa.chunk.css"
+  },
+  {
+    "revision": "3f56d3b9606ef6309dd4",
+    "url": "/portfolio-react/static/js/2.657e99da.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-react/static/js/2.657e99da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad35e04abed861196355",
+    "url": "/portfolio-react/static/js/main.8dc41443.chunk.js"
+  },
+  {
+    "revision": "4c3bcdd375f26df9b5b7",
+    "url": "/portfolio-react/static/js/runtime-main.70e43ac6.js"
+  },
+  {
+    "revision": "3d1b2a223fa1847a4ec7778c6acf92c6",
+    "url": "/portfolio-react/static/media/BudgetTracker.3d1b2a22.png"
+  },
+  {
+    "revision": "c52bb9d4699f46830fb10a7c5d52f6ff",
+    "url": "/portfolio-react/static/media/CurrentResume.c52bb9d4.pdf"
+  },
+  {
+    "revision": "c7b58d29333fe5725d1f826b36b3cfc5",
+    "url": "/portfolio-react/static/media/EmployeeDirectory.c7b58d29.png"
+  },
+  {
+    "revision": "44ae27697947469fe83f67dff4ff9116",
+    "url": "/portfolio-react/static/media/ProfileGenerator.44ae2769.png"
+  },
+  {
+    "revision": "eaeec1386f8038559cf571a514796160",
+    "url": "/portfolio-react/static/media/VirusWars.eaeec138.png"
+  },
+  {
+    "revision": "d9a660c4df995f7f3875897822c79637",
+    "url": "/portfolio-react/static/media/WorkDayScheduler.d9a660c4.png"
+  }
+]);
